70fbb1b29f698400f7dba04a1f77d90218fd6e94
Hamidoo <hussainhafeez1100@gmail.com>
2021-11-24 07:09:30 +0530

    ui fixes

--- src/Components/Learn/Growth.js
+++ src/Components/Learn/Growth.js
@@ -15,7 +15,7 @@ const Growth = () => {
   const stage_3 = reference.map((item) => item.stage_3);
 
   return (
-    <div className="p-5 w-100 position-relative">
+    <div className="conditional-padding w-100 position-relative">
       <div className="p-3 rounded btn-bg-active text-color">
         <h4>{subject}</h4>
         <p>{studyIntro}</p>
@@ -25,104 +25,7 @@ const Growth = () => {
           <button className="btn bg-white my-5">Life skills</button>
         </div>
       </div>
-      <div className="p-3 my-5 rounded btn-bg-active text-color">
-          <i class="process far fa-question-circle icon-z"></i>
-        <h3>{heading}</h3>
-        <p>{subHeading}</p>
-        <ol>
-          <li className="d-flex gap-2 align-items-center">
-            <span>1.</span>
-            <span>{stage_1[0].name}</span>
-            <a
-              href={stage_1[0].ref}
-              target="_blank"
-              rel="noreferrer"
-              className="ref-style"
-            >
-              (www.ted.com)
-            </a>
-          </li>
-          <li className="d-flex gap-2 align-items-center my-2">
-            <span>2.</span>
-
-            <span>{stage_2[0].name}</span>
-            <a
-              href={stage_2[0].ref}
-              target="_blank"
-              rel="noreferrer"
-              className="ref-style"
-            >
-              (www.ted.com)
-            </a>
-          </li>
-          <li className="d-flex gap-2 align-items-center ">
-            <span>3.</span>
-            <span>{stage_3[0].name}</span>
-            <div>
-              <a
-                href={stage_3[0].ref}
-                target="_blank"
-                rel="noreferrer"
-                className="ref-style"
-              >
-                (fs.blog)
-              </a>
-            </div>
-          </li>
-        </ol>
-      </div>
-      {/* 2 */}
-
-      <div className="p-3 my-5 rounded btn-bg-active text-color">
-        <i class="process far fa-question-circle icon-z"></i>
-        <h3>{heading}</h3>
-        <p>{subHeading}</p>
-        <ol>
-          <li className="d-flex gap-2 align-items-center">
-            <span>1.</span>
-            <span>{stage_1[0].name}</span>
-            <a
-              href={stage_1[0].ref}
-              target="_blank"
-              rel="noreferrer"
-              className="ref-style"
-            >
-              (www.ted.com)
-            </a>
-          </li>
-          <li className="d-flex gap-2 align-items-center my-2">
-            <span>2.</span>
-
-            <span>{stage_2[0].name}</span>
-            <a
-              href={stage_2[0].ref}
-              target="_blank"
-              rel="noreferrer"
-              className="ref-style"
-            >
-              (www.ted.com)
-            </a>
-          </li>
-          <li className="d-flex gap-2 align-items-center ">
-            <span>3.</span>
-            <span>{stage_3[0].name}</span>
-            <div>
-              <a
-                href={stage_3[0].ref}
-                target="_blank"
-                rel="noreferrer"
-                className="ref-style"
-              >
-                (fs.blog)
-              </a>
-            </div>
-          </li>
-        </ol>
-      </div>
-
-      {/* 3 */}
-
-      <div className="p-3 my-5 rounded btn-bg-active text-color">
+      <div className="p-3 my-5 rounded btn-bg-active text-color border">
         <i class="process far fa-question-circle icon-z"></i>
         <h3>{heading}</h3>
         <p>{subHeading}</p>
